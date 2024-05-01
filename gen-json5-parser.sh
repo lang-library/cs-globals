4525e86693bfe858b21bbf904f3ef3e6a325fcc1
JavaCommons Technologies <javacommons@gmail.com>
2024-05-01 11:41:33 +0900

    .

--- gen-json5-parser.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -uvx
-set -e
-cwd=`pwd`
-cd $cwd/MyJson
-rm -rf Parser
-java -jar ./antlr-4.13.1-complete.jar JSON5.g4 -Dlanguage=CSharp -package MyJson.Parser.Json5 -o Parser/Json5
--- nunit.myjson.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-set -uvx
-set -e
-cd "$(dirname "$0")"
-cwd=`pwd`
-dotnet test -p:Configuration=Release -p:Platform="Any CPU" MyJson.sln
