dec3601f219068c59ca777e2c66b9de0188cb518
JavaCommons Technologies <javacommons@gmail.com>
2024-05-01 23:00:12 +0900

    .

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@ set -vx
 set -e
 #ownerName=`cat ./OWNER.txt`
 #appName=`cat ./NAME.txt`
-slnName=Globals0
+slnName=ClassLibrary1
 buildMode=Release
 cwd=`pwd`
+cd $cwd/$slnName
 find . -name bin -exec rm -rvf {} +
 find . -name obj -exec rm -rvf {} +
+find . -name packages -exec rm -rvf {} +
 dotnet restore ${slnName}.sln -p:Configuration=${buildMode} -p:Platform="Any CPU"
-msbuild.exe ${slnName}.sln -p:Configuration=${buildMode} -p:Platform="Any CPU"
+dotnet test ${slnName}.sln -p:Configuration=${buildMode} -p:Platform="Any CPU"
+#msbuild.exe ${slnName}.sln -p:Configuration=${buildMode} -p:Platform="Any CPU"
 
 cd $cwd
