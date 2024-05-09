95c104d3e66751fcafc39ac451212486cc893625
JavaCommons Technologies <javacommons@gmail.com>
2024-05-10 06:04:17 +0900

    .

--- Globals.Demo/Program.cs
+++ Globals.Demo/Program.cs
@@ -91,50 +91,19 @@ static class Program
 
         var nlp = new Win32NLJsonParser();
         var nlr = nlp.Parse("""
-            { "a": 123, "b": [11, 22, 33], "c": null }
+            { "a": 123, "b": [11, true, false, null], "c": "hello\nハロー©" }
             """);
         Echo(nlr);
-        Echo(nlr.ast.dict as IDictionary);
-        Echo(nlr.ast.dict as System.Collections.Generic.IDictionary<string, object>);
-        Echo(nlr.ast.dict.GetType().IsGenericType);
-        var tp = nlr.ast.dict.GetType();
-        if (tp.GetInterfaces().Any(
-  i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(System.Collections.Generic.IDictionary<,>)))
+        sw.Start();
+        for (int c = 0; c < 5; c++)
         {
-            Console.WriteLine($"Do something");
+            nlp.Parse(bigJson);
         }
-        var ifc = IsGenericIDictionaryType(tp);
-        Echo(ifc.FullName);
-        if (ifc != null)
-        {
-            ProcessGenericIDictionaryType(nlr.ast.dict);
-        }
-    }
-    public static void ProcessGenericIDictionaryType<T>(System.Collections.Generic.IDictionary<string, T> dict)
-    {
-        foreach(var key in dict.Keys)
-        {
-            Echo(key);
-        }
-
-    }
-    public static Type IsGenericIDictionaryType(Type type)
-    {
-        if (type == null) return null;
-        var ifs = type.GetInterfaces();
-        foreach(var i in ifs )
-        {
-            if (i.IsGenericType && i.GetGenericTypeDefinition() == typeof(System.Collections.Generic.IDictionary<,>))
-            {
-                Type keyType = i.GetGenericArguments()[0];
-                Type valType = i.GetGenericArguments()[1];
-                //if (keyType == typeof(string))
-                {
-                    return valType;
-                }
-
-            }
-        }
-        return null;
+        sw.Stop();
+        Console.WriteLine("■Win32NLJsonParser");
+        ts = sw.Elapsed;
+        Console.WriteLine($"　{ts}");
+        Console.WriteLine($"　{ts.Hours}時間 {ts.Minutes}分 {ts.Seconds}秒 {ts.Milliseconds}ミリ秒");
+        Console.WriteLine($"　{sw.ElapsedMilliseconds}ミリ秒");
     }
 }
